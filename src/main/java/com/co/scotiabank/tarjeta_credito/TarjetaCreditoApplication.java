576d63de2ac682cfd9ec3947bd120f00ae09f73d
eduardo arias <arias_juane@javeriana.edu.co>
2021-04-01 18:26:08 -0500

    first commit

---
+++ src/main/java/com/co/scotiabank/tarjeta_credito/TarjetaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.co.scotiabank.tarjeta_credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/scotiabank/tarjeta_credito/TarjetaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.scotiabank.tarjeta_credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
