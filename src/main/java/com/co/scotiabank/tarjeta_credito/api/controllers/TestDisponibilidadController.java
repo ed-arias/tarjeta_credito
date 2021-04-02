6ae58f5bcb20ddfd576cfd5dcfa0e0d11ff892eb
eduardo arias <arias_juane@javeriana.edu.co>
2021-04-01 19:45:49 -0500

    Fix uri diponibilidad

--- src/main/java/com/co/scotiabank/tarjeta_credito/api/controllers/TestDisponibilidadController.java
+++ src/main/java/com/co/scotiabank/tarjeta_credito/api/controllers/TestDisponibilidadController.java
@@ -3,9 +3,11 @@ package com.co.scotiabank.tarjeta_credito.api.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping("/api/v1")
 public class TestDisponibilidadController {
     
     @GetMapping("/test")
