27cbbc219ce517a92a2166a9a78a55e2273e9133
eduardo arias <arias_juane@javeriana.edu.co>
2021-04-01 20:31:03 -0500

    Change to create database

--- src/main/java/com/co/scotiabank/tarjeta_credito/api/controllers/TestDisponibilidadController.java
+++ src/main/java/com/co/scotiabank/tarjeta_credito/api/controllers/TestDisponibilidadController.java
@@ -1,5 +1,7 @@
 package com.co.scotiabank.tarjeta_credito.api.controllers;
 
+import com.co.scotiabank.tarjeta_credito.api.models.ResponseDisponibilidadModel;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class TestDisponibilidadController {
-    
+
     @GetMapping("/test")
-    public ResponseEntity<String> mostrarRespuestaTest(){
-        return new ResponseEntity<>("test ok", HttpStatus.OK);
+    public ResponseEntity<ResponseDisponibilidadModel> mostrarRespuestaTest() {
+        ResponseDisponibilidadModel disponibilidadModel = new ResponseDisponibilidadModel();
+        disponibilidadModel.setMensaje("Test de disponibilidad exitoso");
+        disponibilidadModel.setStatus(HttpStatus.OK.value());
+        disponibilidadModel.setStatusDescription(HttpStatus.OK.name());
+        return new ResponseEntity<>(disponibilidadModel, HttpStatus.OK);
     }
 }
